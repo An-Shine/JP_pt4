36bb74f047762fd54f512310d31d9751fe6d053c
An-Shine <akh1012@naver.com>
2025-10-17 15:50:24 +0900

    dd

--- Assets/Course Library/Script/SpawnManager.cs
+++ Assets/Course Library/Script/SpawnManager.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     GameObject enemyPrefab;
+    float spawnRange = 9f;
     void Start()
     {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
         Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 }
